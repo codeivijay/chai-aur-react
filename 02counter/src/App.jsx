72e50905df76cea9375a0a18a78d0d0de2195c56
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-05-25 02:51:22 +0530

    useState some concept updates

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -9,8 +9,21 @@ function App() {
   //let counter = 15
   const addValue = function() {
     //counter = counter + 1
+
+  /*setCounter(counter + 1)
     setCounter(counter + 1)
-    
+    setCounter(counter + 1)
+    setCounter(counter + 1) */
+
+    // this is the situation with useState. as it sends these actions in batches and when it see it had to do same task 
+    // 
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+
+    //which accepts call back function by default.(hidden feature)
+   
   }
 
   const removeValue = () => {
