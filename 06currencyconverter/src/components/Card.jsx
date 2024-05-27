8fbd06e235085a52ff8745865cfbd11e3aec2de2
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-05-27 05:55:41 +0530

    did some debugging in currency converter

---
+++ 06currencyconverter/src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Card() {
+  return (
+    
+      <div className=" w-96 bg-purple-800 shadow-xl">
+        <figure className=" w-25  h-30 rounded-lg ">
+          <img
+            src="https://images.pexels.com/photos/7680637/pexels-photo-7680637.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Shoes"
+          />
+        </figure>
+        
+      </div>
+    
+  );
+}
+
+export default Card;
---
+++ 06currencyconverter/src/components/index.js
@@ -0,0 +1,5 @@
+import InputBox from "./InputBox";
+import Card from "./Card";
+
+
+export {InputBox, Card}
